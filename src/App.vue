4d18cd904c2a56a4442ec7d5127bd22aad52fa8b
丘惟夫 <waveciou@gmail.com>
2019-07-12 01:41:33 +0800

    計時器完成

--- src/App.vue
+++ src/App.vue
@@ -3,26 +3,33 @@
     <div class="wrapper" :class="{'theme-green': this.statusBreak === true}">
       <div class="views-zone">
         <div class="pomodoro">
+
           <svg id="svg" width="100%" height="100%" viewPort="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
-            <circle id="bar" :r="this.circle.radius" cx="330" cy="330" fill="transparent" stroke-dasharray="958.18" stroke-dashoffset="0" :class="{'is-anamate': this.circle.isAnamate === true}"></circle>
+            <circle id="bar" :r="this.circle.radius" cx="330" cy="330" fill="transparent" stroke-dasharray="958.18" stroke-dashoffset="0" @transitionend="circleTransitionEnd()" :class="{'is-animate': this.circle.isAnimate === true}"></circle>
           </svg>
+
+          <!-- <div class="pomodoro__circle" :class="{'is-animate': this.circle.isAnimate === true }"></div> -->
+
           <div class="pomodoro__content">
             <div class="pomodoro__caption">
-              <div class="pomodoro__time"><span class="min">25</span>:<span class="sec">00</span></div>
-              <div class="pomodoro__ctrlbar">
-                <a href="javascript:;" class="pomodoro__playbtn" @click.stop="playCountdown()" :class="{'is-pause': this.activeTimer.isActive === true}">
-                  <span v-if="this.activeTimer.isActive === false">START</span>
+              <div class="pomodoro__time"><span class="min">{{getMinuteText}}</span>:<span class="sec">{{getSecondText}}</span></div>
+              <div class="pomodoro__controlBar">
+                <a href="javascript:;" class="pomodoro__mainButton" @click.stop="playCountdownTimer()" :class="{'is-pause': this.countdownTimer.isActive === true}">
+                  <span v-if="this.countdownTimer.isActive === false">START</span>
                   <span v-else>PAUSE</span>
+                </a>
+                <a href="javascript:;" class="pomodoro__clearButton" @click.stop="clearCountdownTimer()">
+                  <i class="material-icons">close</i>
                 </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="ctrl-zone">
+      <div class="control-zone">
         <div class="wrap">
           <transition name="fade" mode="out-in">
-            <router-view />
+            <router-view :countdownList="countdownList" />
           </transition>
         </div>
       </div>
@@ -50,23 +57,35 @@
   import todolist from './views/todolist.vue';
   import analytics from './views/analytics.vue';
   import ringtone from './views/ringtone.vue';
+  import { setTimeout } from 'timers';
 
   export default {
     data() {
       return {
-        activeTimer: {
-          name: '第一件待辦事項是做一個蕃茄鐘',
+        countdownTimer: {
+          name: '',
           level: 0,
           isActive: false,
-          time: 60,
-          timerContainer: null,
+          time: 0,
+          timer: null,
         },
+        countdownList: [
+          {
+            name: '第一件待辦事項是做一個蕃茄鐘',
+            level: 0
+          },
+          {
+            name: '第二件待辦事項再做一個蕃茄鐘',
+            level: 3
+          }
+        ],
         circle: {
           dom: null,
           radius: 305,
-          isAnamate: false
+          isAnimate: true
         },
         statusBreak: false,
+        isFinish: true
       }
     },
     components: {
@@ -79,61 +98,107 @@
     },
     mounted() {
       this.circle.dom = document.querySelector('#bar');
+
+      this.countdownTimer.name = this.countdownList[0].name;
+      this.countdownTimer.level = this.countdownList[0].level;
+
+      let data = this.getLevelData(this.countdownTimer.level);
+      this.countdownTimer.time = data.totleTime;
+      this.statusBreak = data.status;
     },
     methods: {
-      playCountdown() {
-        this.activeTimer.isActive = !this.activeTimer.isActive;
-        this.activeCountdown(this.activeTimer.isActive);
+      circleTransitionEnd() {
+        if(this.isFinish === true) {
+          this.countdownTimer.level += 1;
+          this.countdownList[0].level = this.countdownTimer.level;
+          this.clearCountdownTimer();
+          this.isFinish = false;
+        }
       },
-      activeCountdown(status) {
-        if(status === true) {
-          let data = this.getLevelData(this.activeTimer.level);
-          let sum = data.totletime;
+      playCountdownTimer() {
+        this.countdownTimer.isActive = !this.countdownTimer.isActive;
+        this.setCountdownTimer();
+      },
+      setCountdownTimer() {
+        if(this.countdownTimer.isActive === true) {
+          let data = this.getLevelData(this.countdownTimer.level);
+          let sum = data.totleTime;
+          this.isFinish = false;
 
-          this.activeTimer.timerContainer = setInterval(() => {
-            this.activeTimer.time --;
-            this.circle.isAnamate = true;
-
-            let percent = Math.floor(this.activeTimer.time / sum * 100);
-            let c = Math.PI * (this.circle.radius * 2);
+          this.countdownTimer.timer = setInterval(() => {
+            this.countdownTimer.time --;
+            
+            let percent = Math.floor(this.countdownTimer.time / sum * 100);
             if (percent < 0) percent = 0;
             if (percent > 100) percent = 100;
+
+            let c = Math.PI * (this.circle.radius * 2);
             let pct = ((100 - percent) / 100 * c)/-2;
+
             this.circle.dom.style.strokeDashoffset = pct;
 
-            if(this.activeTimer.time <= 0) {
-              this.activeTimer.level += 1;
-              this.activeTimer.isActive = false;
-              this.circle.isAnamate = false;
-              this.circle.dom.style.strokeDashoffset = 0;
-              
-              let data = this.getLevelData(this.activeTimer.level);
-              this.activeTimer.time = data.totletime;
-              this.statusBreak = data.status;
-
-              clearInterval(this.activeTimer.timerContainer);
-              return false
+            if(this.countdownTimer.time <= 0) {
+              this.isFinish = true;
             }
+
+            // let deg = (100 - percent) * 1.8;
+            // if(this.countdownTimer.time <= 0) {
+            //   deg = 180;
+            //   isFinish = true;
+            // }
+            // this.circle.dom.style.transform = `rotateZ(${deg}deg)`;
+            // if(isFinish === true) {
+            //   this.countdownTimer.level += 1;
+            //   this.clearCountdownTimer();
+            // }
+
           }, 1000);
+
         } else {
-          this.circle.isAnamate = false;
-          clearInterval(this.activeTimer.timerContainer);
+          this.countdownTimer.isActive = false;
+          clearInterval(this.countdownTimer.timer);
+          return false
         }
       },
+      clearCountdownTimer() {
+        this.isFinish = false;
+        this.countdownTimer.isActive = false;
+        this.circle.isAnimate = false;
+
+        clearInterval(this.countdownTimer.timer);
+
+        let data = this.getLevelData(this.countdownTimer.level);
+        this.countdownTimer.time = data.totleTime;
+
+        this.circle.dom.style.strokeDashoffset = 0;
+        setTimeout(() => {
+          this.circle.isAnimate = true;
+        }, 1000);
+
+        this.statusBreak = data.status;
+        
+      },
       getLevelData(level) {
-        let obj = {}
-        if(level % 2 === 0) {
-          obj.totletime = 60;
-          obj.status = false;
-        } else {
-          obj.totletime = 30;
-          obj.status = true;
-        }
+        let t, s, obj = {};
+        level % 2 === 0 ? [t, s] = [10, false] : [t, s] = [5, true];
+        obj.totleTime = t;
+        obj.status = s;
         return obj;
       }
     },
     computed: {
-
+      getMinuteText() {
+        let text = '';
+        let val = Math.floor(this.countdownTimer.time / 60);
+        val < 10 ? text = `0${val}` : text = `${val}`;
+        return text;
+      },
+      getSecondText() {
+        let text = '';
+        let val = this.countdownTimer.time % 60;
+        val < 10 ? text = `0${val}` : text = `${val}`;
+        return text;
+      }
     },
     watch: {
 
@@ -142,7 +207,7 @@
 </script>
 
 <style lang="scss">
-  @import 'assets/scss/utils/_utils.scss';
+  @import 'assets/scss/_utils.scss';
   @import 'assets/scss/main.scss';
 
 </style>
--- src/views/todolist.vue
+++ src/views/todolist.vue
@@ -1,13 +1,79 @@
 <template>
     <div class="content">
-        <h2>TODOLIST</h2>
+        <h2>THE MISSION TODAY</h2>
+        <ul class="countdownList">
+            <li v-for="(item, index) in countdownList" :key="index">
+                <a href="javascript:;" class="countdownList__item">
+                    <div class="countdownList__title">{{ item.name }}</div>
+                    <div class="countdownList__level">
+                        <span v-for="(star, i) in item.level" :key="i" ></span>
+                    </div>
+                </a>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+
+            }
+        },
         name: 'todolist',
-        components: {
+        props: ['countdownList'],
+        created() {
+
+        },
+        mounted() {
+
         }
     }
 </script>
+
+<style lang="scss">
+    @import '../assets/scss/_utils.scss';
+
+    .countdownList {
+        > li {
+            margin: 15px 0px;
+        }
+    }
+
+    .countdownList__item {
+        display: block;
+    }
+
+    .countdownList__title {
+        margin: 10px 0px;
+        padding-left: 30px;
+        line-height: 21px;
+        position: relative;
+
+        &::before {
+            content: '';
+            width: 17px;
+            height: 17px;
+            display: block;
+            border: 2px $color-black solid;
+            border-radius: 100%;
+            position: absolute;
+            left: 0px;
+        }
+    }
+
+    .countdownList__level {
+        width: 100%;
+        display: flex;
+        padding-left: 30px;
+        > span {
+            width: 12px;
+            height: 12px;
+            display: block;
+            margin-right: 7.5px;
+            border-radius: 100%;
+            background-color: $color-black;
+        }
+    }
+</style>
